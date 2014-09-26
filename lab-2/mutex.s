e54ae11353070f90badc308d8386b23010d0b356
fohne <advancefohn@hotmail.com>
2014-09-27 00:05:29 +0800

    lab-2 finished

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -8,18 +8,31 @@
 	.global lock_mutex
 	.type lock_mutex, function
 lock_mutex:
-        @ INSERT CODE BELOW
-
+	@ INSERT CODE BELOW
+	 ldr r1, =locked
+.Lock:
+	ldrex r2, [r0]
+	cmp r2, #0
+		strexeq r2,r1, [r0]
+		cmpeq r2, #0
+		bne .Lock
+	
         @ END CODE INSERT
 	bx lr
 
 	.size lock_mutex, .-lock_mutex
 
+
+
+
+
 	.global unlock_mutex
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+	ldr r1, =unlocked
+	str r1, [r0]
+   
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
